2093fd56ea0f3f1d886a95c206b3a29a1863e046
Augusto Villegas <villevip10@gmail.com>
2025-10-28 12:38:22 -0300

    Actualizacion integral22

--- src/components/ui/EmptyRow.jsx
+++ src/components/ui/EmptyRow.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 export const EmptyRow = ({ columns }) => (
   <tr>
     <td
       colSpan={columns?.length || 1}
-      className="p-6 text-center text-sm text-gray-100 dark:text-gray-300 font-mono"
+      className="p-6 text-center text-sm text-gray-500 dark:text-gray-300 font-mono"
     >
       No hay registros.
     </td>
